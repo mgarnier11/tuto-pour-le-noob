a836c20f66d0e5f1d733ad0194472ca070fda472
mgarnier11 <mgarnier11@gmail.com>
2022-05-26 01:43:32 +0200

    feat: hardened tuto

--- src/TaskItem.js
+++ src/TaskItem.js
@@ -4,11 +4,7 @@ import './taskItem.css'
 function TaskItem({onClose, open, title, description}) {
 
   return (
-    <Modal modalLable='Task Item' onClose={onClose} open={open}>
-      <div className='taskItem'>
-        <h2>{title}</h2>
-        <p>{description}</p>
-      </div>
+    <Modal modalLable='Task Item'>
     </Modal>
   )
 }
